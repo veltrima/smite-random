3cbdd1cc625a974c28b405a6e84b95360e6b3e32
veltrima <matthew.veltri@mail.utoronto.ca>
2018-03-15 07:30:39 -0400

    First commit

---
+++ random.js
@@ -0,0 +1,584 @@
+// Because statis pages don't like files, and csv is a pain to copy/paste
+var god_list = [
+ {
+   "Name": "Arachne",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Awilix",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Bakasura",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Bastet",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Camazotz",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Da Ji",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Fenrir",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Hun Batz",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Kali",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Loki",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Mercury",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Ne Zha",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Nemesis",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Ratatoskr",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Serqet",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Susano",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Thanatos",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Thor",
+   "Role": "Assassin",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Achilles",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Amaterasu",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Bellona",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Chaac",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Cu Chulainn",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Erlang Shen",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Guan Yu",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Hercules",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Nike",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Odin",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Osiris",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Ravana",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Sun Wukong",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Tyr",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Vamana",
+   "Role": "Warrior",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Ah Muzen Cab",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Anhur",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Apollo",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Artemis",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Cernunnos",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Chiron",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Cupid",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Hachiman",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Hou Yi",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Izanami",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Jing Wei",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Medusa",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Neith",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Rama",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Skadi",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Ullr",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Xbalanque",
+   "Role": "Hunter",
+   "Type": "Physical"
+ },
+ {
+   "Name": "Agni",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Ah Puch",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Anubis",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Ao Kuang",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Aphrodite",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Chang'e",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Chronos",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Discordia",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Freya",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Hades",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "He Bo",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Hel",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Isis",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Janus",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Kukulkan",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Nox",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Nu Wa",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Poseidon",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Ra",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Raijin",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Scylla",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Sol",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "The Morrigan",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Thoth",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Vulcan",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Zeus",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Zhong Kui",
+   "Role": "Mage",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Ares",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Artio",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Athena",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Bacchus",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Cabrakan",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Cerberus",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Fafnir",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Ganesha",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Geb",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Khepri",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Kumbhakarna",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Kuzenbo",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Sobek",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Sylvanus",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Terra",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Xing Tian",
+   "Role": "Guardian",
+   "Type": "Magical"
+ },
+ {
+   "Name": "Ymir",
+   "Role": "Guardian",
+   "Type": "Magical"
+ }
+];
+
+$(document).ready(function(){
+    $("#god-selection").html(generateGodCheckboxes());
+    $("#random").click(function(){
+        var selected = getRandom();
+        if (selected !== null) {
+          $("#chosen").text(selected);
+          $("#god-card").attr("src", getGodCard(selected));
+          $("#god-card").show();
+        }
+        else {
+          $("#chosen").text("Oops, change your criteria!");
+          $("#god-card").attr("src", "");
+          $("#god-card").hide();
+        }
+    });
+    $("#reset-role").click(function(){
+        $("input[name=role]").prop("checked", true);
+    });
+    $("#reset-type").click(function(){
+        $("input[name=type]").prop("checked", true);
+    });
+    $("#reset-god-selection").click(function(){
+        $("input[name=god]").prop("checked", true);
+    });
+    $("#toggle-god-selection").click(function(){
+      $("#god-selection").toggle();
+      $("#reset-god-selection").toggle();
+      if ($("#god-selection").is(":visible")) {
+        $("#toggle-god-selection").text("Hide God Selection");
+      }
+      else {
+        $("#toggle-god-selection").text("Show God Selection");
+      }
+    });
+});
+
+// Alphebetizes by name
+// Thank you https://stackoverflow.com/questions/881510/sorting-json-by-values
+function sortJSON(data) {
+    return data.sort(function(a, b) {
+        var x = a.Name; var y = b.Name;
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+
+function generateGodCheckboxes() {
+  var html = "";
+  var sorted_gods = sortJSON(god_list);
+  for (var i=0; i<sorted_gods.length; i++) {
+    html += "<input type=\"checkbox\" name=\"god\" value=\"" + sorted_gods[i].Name + "\" checked>" + sorted_gods[i].Name + "<br>";
+  }
+  return html;
+}
+
+function getGodCard(god) {
+  god = god.replace(" ", "-");
+  god = god.replace("'", "");
+  return "https://smitefire.com/images/god/card/" + god + ".png";
+}
+
+// Pass the checkbox name to the function
+// Thanks https://stackoverflow.com/questions/8563240/how-to-get-all-checked-checkboxes
+function getCheckedBoxes(chkboxName) {
+  // Could use jQuery but this works fine
+  var checkboxes = document.getElementsByName(chkboxName);
+  var checkboxesChecked = [];
+  // loop over them all
+  for (var i=0; i<checkboxes.length; i++) {
+     // And stick the checked ones onto an array...
+     if (checkboxes[i].checked) {
+        checkboxesChecked.push(checkboxes[i].value);
+     }
+  }
+  return checkboxesChecked;
+}
+
+function filterByGodSelect(name_list, god_pool = god_list) {
+  var filtered=god_pool.filter(function(item){
+    var result = $.inArray(item.Name, name_list);
+    return result !== -1;
+  });
+  return filtered;
+}
+
+function filterByRole(role_list, god_pool = god_list) {
+  var filtered=god_pool.filter(function(item){
+    var result = $.inArray(item.Role, role_list);
+    return result !== -1;
+  });
+  return filtered;
+}
+
+function filterByType(type_list, god_pool = god_list) {
+  var filtered=god_pool.filter(function(item){
+    var result = $.inArray(item.Type, type_list);
+    return result !== -1;
+  });
+  return filtered;
+}
+
+function getRandom() {
+  var god_pool = filterByGodSelect(getCheckedBoxes("god"));
+  god_pool = filterByRole(getCheckedBoxes("role"), god_pool);
+  god_pool = filterByType(getCheckedBoxes("type"), god_pool);
+  if (god_pool.length === 0) {
+    return null;
+  }
+  var chosen_god = Math.floor((Math.random() * god_pool.length));
+  return god_pool[chosen_god].Name;
+}
